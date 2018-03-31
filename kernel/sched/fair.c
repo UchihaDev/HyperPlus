4972dd43f5f025b0bf7db5a9aa5f8ad3f4ccd803
UchihaDev <ajit4guraya@gmail.com>
2018-03-31 16:31:15 +0545

    sched/fair: Optimize find_idlest_cpu() when there is no choice""
    This reverts commit 36465a2faa9f26af91a7fd8caddff807dbee4901.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6135,6 +6135,10 @@ find_boost_cpu(struct cpumask *group_cpus, struct task_struct *p, int this_cpu)
 	int shallowest_idle_cpu = -1;
 	int i;
 
+	/* Check if we have any choice: */
+	if (group->group_weight == 1)
+		return cpumask_first(sched_group_cpus(group));
+
 	/* Traverse only the allowed CPUs */
 	for_each_cpu_and(i, group_cpus, tsk_cpus_allowed(p)) {
 		if (!cpumask_test_cpu(i, cpu_online_mask))
