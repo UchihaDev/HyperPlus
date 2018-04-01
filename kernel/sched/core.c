432dd79248ed7a19e1193c215a5e5089fefed1c3
UchihaDev <ajit4guraya@gmail.com>
2018-04-01 20:42:48 +0545

    Revert "sched/deadline: Avoid pointless __setscheduler()"
    This reverts commit 867e95607f1f2852fa42552e4ed0cc3127fdead0.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4056,20 +4056,6 @@ static bool dl_param_changed(struct task_struct *p,
 	return false;
 }
 
-static bool dl_param_changed(struct task_struct *p,
-		const struct sched_attr *attr)
-{
-	struct sched_dl_entity *dl_se = &p->dl;
-
-	if (dl_se->dl_runtime != attr->sched_runtime ||
-		dl_se->dl_deadline != attr->sched_deadline ||
-		dl_se->dl_period != attr->sched_period ||
-		dl_se->flags != attr->sched_flags)
-		return true;
-
-	return false;
-}
-
 static int __sched_setscheduler(struct task_struct *p,
 				const struct sched_attr *attr,
 				bool user, bool pi)
