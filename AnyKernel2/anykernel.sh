7569fdf864ed978ce7bf32b9e656f3b7f5b545f4
UchihaDev <ajit4guraya@gmail.com>
2018-04-16 18:11:16 +0545

    Add some defined values for kernel settings

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -45,10 +45,51 @@ ui_print "Adding flavor to the kernel...";
 
 dump_boot;
 
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+# add default values 1
+insert_line init.rc '# set default schedTune value for foreground/top-app (only affects EAS)' 'before' '# Mount staging areas for devices managed by vold' '# set default schedTune value for foreground/top-app (only affects EAS)'
+insert_line init.rc 'write /dev/stune/foreground/schedtune.prefer_idle 1' 'after' '# set default schedTune value for foreground/top-app (only affects EAS)' 'write /dev/stune/foreground/schedtune.prefer_idle 1'
+insert_line init.rc 'write /dev/stune/top-app/schedtune.prefer_idle 1' 'after' 'write /dev/stune/foreground/schedtune.prefer_idle 1' 'write /dev/stune/top-app/schedtune.prefer_idle 1'
+insert_line init.rc 'write /dev/stune/top-app/schedtune.boost 10' 'after' 'write /dev/stune/top-app/schedtune.prefer_idle 1' 'write /dev/stune/top-app/schedtune.boost 10'
+
+# add default values 2
+insert_line init.rc '# update cpusets now that processors are up' 'before' '# Start standard binderized HAL daemons' '# update cpusets now that processors are up'
+insert_line init.rc 'write /dev/cpuset/top-app/cpus 0-7' 'after' '# update cpusets now that processors are up' 'write /dev/cpuset/top-app/cpus 0-7'
+insert_line init.rc 'write /dev/cpuset/foreground/cpus 0-7' 'after' 'write /dev/cpuset/top-app/cpus 0-7' 'write /dev/cpuset/foreground/cpus 0-7'
+insert_line init.rc 'write /dev/cpuset/foreground/boost/cpus 0-7' 'after' 'write /dev/cpuset/foreground/cpus 0-7' 'write /dev/cpuset/foreground/boost/cpus 0-7'
+insert_line init.rc 'write /dev/cpuset/background/cpus 0-7' 'after' 'write /dev/cpuset/foreground/boost/cpus 0-7' 'write /dev/cpuset/background/cpus 0-7'
+insert_line init.rc 'write /dev/cpuset/system-background/cpus 0-7' 'after' 'write /dev/cpuset/background/cpus 0-7' 'write /dev/cpuset/system-background/cpus 0-7'
+insert_line init.rc '# set default schedTune value for foreground/top-app (only affects EAS)' 'after' 'write /dev/cpuset/system-background/cpus 0-7' '# set default schedTune value for foreground/top-app (only affects EAS)'
+insert_line init.rc 'write /dev/stune/foreground/schedtune.prefer_idle 1' 'after' '# set default schedTune value for foreground/top-app (only affects EAS)' 'write /dev/stune/foreground/schedtune.prefer_idle 1'
+insert_line init.rc 'write /dev/stune/top-app/schedtune.prefer_idle 1' 'after' 'write /dev/stune/foreground/schedtune.prefer_idle 1' 'write /dev/stune/top-app/schedtune.prefer_idle 1'
+insert_line init.rc 'write /dev/stune/top-app/schedtune.boost 1' 'after' 'write /dev/stune/top-app/schedtune.prefer_idle 1' 'write /dev/stune/top-app/schedtune.boost 1'
+
 # make schedutil default governor
 insert_line init.rc '# set governor' 'before' '# scheduler tunables' '# set governor'
 insert_line init.rc 'write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "schedutil"' 'after' '# set governor' 'write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "schedutil"'
 insert_line init.rc 'write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor "schedutil"' 'after' 'write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "schedutil"' 'write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor "schedutil"'
+
+# add default values 3
+insert_line init.rc 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us 500' 'after' 'write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor "schedutil"' 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us 500'
+insert_line init.rc 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us 20000' 'after' 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us 500' 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us 20000'
+insert_line init.rc 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/iowait_boost_enable 1' 'after' 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us 20000' 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/iowait_boost_enable 1'
+insert_line init.rc 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us 500' 'after' 'write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/iowait_boost_enable 1' 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us 500'
+insert_line init.rc 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us 20000' 'after' 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us 500' 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us 20000'
+insert_line init.rc 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/iowait_boost_enable 1' 'after' 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us 20000' 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/iowait_boost_enable 1'
+insert_line init.rc 'write /sys/module/cpu_boost/parameters/input_boost_freq "0:1036800"' 'after' 'write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/iowait_boost_enable 1' 'write /sys/module/cpu_boost/parameters/input_boost_freq "0:1036800"'
+insert_line init.rc 'write /sys/module/cpu_boost/parameters/dynamic_stune_boost 15' 'after' 'write /sys/module/cpu_boost/parameters/input_boost_freq "0:1036800"' 'write /sys/module/cpu_boost/parameters/dynamic_stune_boost 15'
+insert_line init.rc 'write /sys/module/cpu_boost/parameters/input_boost_ms 500' 'after' 'write /sys/module/cpu_boost/parameters/dynamic_stune_boost 15' 'write /sys/module/cpu_boost/parameters/input_boost_ms 500'
+insert_line init.rc 'write /dev/cpuset/top-app/cpus 0-7' 'after' 'write /sys/module/cpu_boost/parameters/input_boost_ms 500' 'write /dev/cpuset/top-app/cpus 0-7'
+insert_line init.rc 'write /dev/cpuset/foreground/boost/cpus 0-3,6-7' 'after' 'write /dev/cpuset/top-app/cpus 0-7' 'write /dev/cpuset/foreground/boost/cpus 0-3,6-7'
+insert_line init.rc 'write /dev/cpuset/background/cpus 0-1' 'after' 'write /dev/cpuset/foreground/boost/cpus 0-3,6-7' 'write /dev/cpuset/background/cpus 0-1'
+insert_line init.rc 'write /dev/cpuset/system-background/cpus 0-3' 'after' 'write /dev/cpuset/background/cpus 0-1' 'write /dev/cpuset/system-background/cpus 0-3'
+insert_line init.rc 'write /sys/block/sda/queue/read_ahead_kb 1024' 'after' 'write /dev/cpuset/system-background/cpus 0-3' 'write /sys/block/sda/queue/read_ahead_kb 1024'
+insert_line init.rc 'write /sys/block/sda/queue/nr_requests 128' 'after' 'write /sys/block/sda/queue/read_ahead_kb 1024' 'write /sys/block/sda/queue/nr_requests 128'
+insert_line init.rc 'write /sys/block/sda/queue/iostats 1' 'after' 'write /sys/block/sda/queue/nr_requests 128' 'write /sys/block/sda/queue/iostats 1'
 
 write_boot;
 
@@ -65,8 +106,6 @@ mv /tmp/anykernel/kernel-Image.gz /tmp/anykernel/Image.gz;
 
 
 ## AnyKernel kernel install
-
-ui_print "Adding a little more flavor in there...";
 
 split_boot;
 
