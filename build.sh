8d3dd3cc2a456b062f1ac57487e23da48e721979
UchihaDev <ajit4guraya@gmail.com>
2018-04-18 21:45:22 +0545

    edit paths in build script

--- build.sh
+++ build.sh
@@ -101,7 +101,7 @@ case $ch in
      echo -e "***********************************************$nocol"
      make ARCH=arm64 distclean
      rm -rf ../out
-     cp -r ~/Desktop/Files/Source_files/libperl.a drivers/huawei_platform/lcd/tools/localperl/lib/5.14.2/x86_64-linux-thread-multi/CORE/libperl.a
+     cp -r ~/Desktop/Files/Source_files/libperl.a drivers/huawei_platform/lcd/tools/localperl/lib/5.14.2/x86_64-linux-thread-multi/CORE/libper$
      cp -r ~/Desktop/Files/Source_files/zconf.hash.c scripts/kconfig/zconf.hash.c
      cp -r ~/Desktop/Files/Source_files/zconf.lex.c scripts/kconfig/zconf.lex.c
      cp -r ~/Desktop/Files/Source_files/zconf.tab.c scripts/kconfig/zconf.tab.c
